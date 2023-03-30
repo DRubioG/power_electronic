99f88840b9ff530afdb5b0e4de01e9aaf4f72743
David R <davidrubio97@outlook.com>
2023-03-30 18:14:12 +0200

    update

--- transformations.py
+++ transformations.py
@@ -8,6 +8,7 @@ def abc_2_ab(abc):
 	ab = np.matmul(mat_clarke, abc)
 	return ab
 	
+
 def ab_2_abc(ab):
 	mat_clarke_inv = [[1, 0, 1],
 		   			  [-1/2, np.sqrt(3)/2, 1],
@@ -16,6 +17,7 @@ def ab_2_abc(ab):
 	abc = np.matmul(mat_clarke_inv, ab)
 	
 	return abc
+
 
 def dq_2_ab(ab, theta):
 	mat_park = [[np.cos(theta), np.sin(theta), 0],
@@ -26,6 +28,7 @@ def dq_2_ab(ab, theta):
 
 	return dq
 
+
 def ab_2_dq(dq, theta):
 	mat_clark_inv = [[np.cos(theta), -np.sin(theta), 0],
 		  			[np.sin(theta), np.cos(theta), 0],
@@ -35,11 +38,13 @@ def ab_2_dq(dq, theta):
 
 	return ab
 
+
 def abc_2_dq(abc, theta):
 	ab = abc_2_ab(abc)
 	dq = dq_2_ab(ab, theta)
 
 	return dq
+
 
 def dq_2_abc(dq, theta):
 	ab = dq_2_ab(dq, theta)
